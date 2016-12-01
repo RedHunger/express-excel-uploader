d0cd4b6f94bfc572a085124441621b652075a1aa
igor <i.golod@andersenlab.com>
2016-12-01 19:00:43 +0200

    add handlebars template

--- server.js
+++ server.js
@@ -1,37 +1,54 @@
-var http = require('http');
-var fs = require("fs");
-var data = 'Update current file';
+var http = require('http'),
+    fs = require("fs"),
+    path = require('path');
+    express = require('express'),
+    fileUpload = require('express-fileupload');
+    app = express(),
+    data = 'Update current file';
+    bodyParser = require('body-parser');
+    handlebars = require('express-handlebars');
 
-// Create a readable stream
-var readerStream = fs.createReadStream('file.txt');
-var writerStream = fs.createWriteStream('file.txt');
-var server = http.createServer(function(req, res) {
-    // Set the encoding to be utf8. 
-    readerStream.setEncoding('UTF8');
+app.engine('handlebars', handlebars({extname:'handlebars', defaultLayout: 'layout', layoutsDir: __dirname + '/views/'}));
+app.set('views',path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
 
-    // Handle stream events --> data, end, and error
-    readerStream.on('data', function(chunk) {
-        data += chunk;
+app.use('/form', express.static(__dirname + '/upload.test.html'));
+app.use(fileUpload());
+
+app.post('/upload', function(req, res) {
+
+    var sampleFile; 
+    if (!req.files) {
+        res.send('No files were uploaded.');
+        return;
+    }
+    sampleFile = req.files.sampleFile;
+    sampleFile.mv(__dirname +"/uppload/"+ req.files.sampleFile["name"], function(err) {
+        if (err) {
+            res.status(500).send(err);
+        }
+        else {
+            fs.readdir(__dirname + "/uppload/", (err, files) => {
+                files.forEach(file => {
+                    res.render('layout',{title:'OK', anyArray : files});
+                    console.log("Files:"+ file);
+                });
+            });
+
+            // res.render('layout',{title:'OK', body :"File uploaded"});
+        }
     });
 
-    readerStream.on('end', function() {
-        console.log(data);
-    });
 
-    readerStream.on('error', function(err) {
-        console.log(err.stack);
-    });
-
-    writerStream.write(data,'UTF8');
-
-    writerStream.on('finish', function() {
-        console.log("Write completed.");
-    });
-
-    writerStream.on('error', function(err) {
-        console.log(err.stack);
-    });
 
 });
-server.listen(8000);
+
+app.use(function (req,res,next){
+    res.send(404,"Sorry! Page not found");
+});
+
+
 console.log("Go to server");
+console.log("-------------------");
+app.listen(8000);
+
