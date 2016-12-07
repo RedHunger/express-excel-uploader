4bc0a6eadcfb842fae712e63178782d58f6008d6
igor <i.golod@andersenlab.com>
2016-12-07 12:59:29 +0200

    add app

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,65 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var XLSX = require('xlsx');
+var data = [];
+var jsonData = [];
+var testData = 0;
+var forEach = require('lodash.foreach');
+
 router.use('/*', express.static('public/index.html'));
 router.use('/upload', fileUpload());
+router.get('/list', function(req, res) {
+    res.json(jsonData);
+});
 router.post('/upload', function(req, res) {
     var sampleFile;
+    sampleFile = req.files;
     if (!req.files) {
         res.send('No files were uploaded.');
         return;
     }
     sampleFile = req.files.sampleFile;
-    sampleFile.mv(__dirname +"/uppload/"+ req.files.sampleFile["name"], function(err) {
+    sampleFile.mv("./uppload/"+ req.files.sampleFile["name"], function(err) {
+
+        var workbook = XLSX.readFile('./uppload/' + req.files.sampleFile["name"]);
+        var inputData = workbook.Sheets.Лист1;
+        var testData = 0;
+        var column = '';
+        function searchColumn(input){
+            forEach(input, function(value, key){
+                if (value.v === 'Стоимость') {
+                    column = key.charAt(0);
+                }
+            })
+        }
+        function test (input) {
+            forEach(input, function(value, key) {
+                if(key.charAt(0) === column){
+                    if(typeof(value.v) === 'number'){
+                        testData += value.v
+                    }
+                }
+            });
+        }
+
+        searchColumn(inputData);
+        test(inputData);
+
+        data.push({
+            name: req.files.sampleFile["name"],
+            browse: 'uppload/' + req.files.sampleFile["name"],
+            sum: testData
+        });
+
+        jsonData = JSON.stringify(data);
+
         if (err) {
             res.status(500).send(err);
         }
         else {
-
-
-
+            res.send('File uploaded!');
+            
         }
     });
 
