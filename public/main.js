498263edaf9cb58738d5ab71418bf89bbd0a57fa
igor <i.golod@andersenlab.com>
2016-12-15 19:24:14 +0200

    update js file

--- public/main.js
+++ public/main.js
@@ -1,18 +1,20 @@
-$(window).on('load',function () {
-    function ajaxInfo() {
-        $.ajax({
-            type: 'POST',
-            contentType: 'application/json',
-            url: 'http://127.0.0.1:8000/info',
-            success: function(response) {
-                $('.list').html('');
-                var storage = response.files;
-                for (val in storage) {
-                    $('.list').append('<li>'+'Name: '+storage[val].name + '<br>' + ' TimeStamp: '  + storage[val].time + '</li>');
-                }
+function ajaxInfo() {
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/json',
+        url: 'http://127.0.0.1:8000/info',
+        success: function(response) {
+            $('.list').html('');
+            var storage = response.files;
+            for (val in storage) {
+                $('.list').append('<li>'+'Name: '+storage[val].name + '<br>' + ' TimeStamp: '  + storage[val].time + '</li>');
             }
-        });
-    }
+        }
+    });
+};
+
+$(window).on('load',function () {
+
 
     ajaxInfo();
 
@@ -20,10 +22,10 @@ $(window).on('load',function () {
 $(document).ready(function () {
     $('#sendFileButton').on('click', function (e) {
         e.preventDefault();
-        var data = {
-            fileName : $('#fileField').get(0).files[0].name,
-            fileTimeStamp: $('#fileField').get(0).files[0].lastModified,
-        };
+        var data = new FormData();
+        $.each($('#fileField')[0].files, function(i, file) {
+            data.append('file-'+i, file);
+        });
         $.ajax({
             type: 'POST',
             data: JSON.stringify(data),
@@ -39,8 +41,10 @@ $(document).ready(function () {
             $("#uploadForm").submit(),
             $.ajax({
                 type: 'POST',
-                data: JSON.stringify(data),
-                contentType: 'application/json',
+                data: data,
+                cache: false,
+                contentType: false,
+                processData: false,
                 url: 'http://127.0.0.1:8000/upload',
                 success: function(data) {
                     alert('ulr: upload');
